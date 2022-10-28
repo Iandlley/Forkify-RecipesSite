0bdfd0204b6b5efac5006259ed8d5c42b95c825d
Iandlley <105615079+Iandlley@users.noreply.github.com>
2022-10-28 09:34:58 -0300

    Add files via upload

---
+++ dist/index.62406edb.js
@@ -0,0 +1,22 @@
+const recipeContainer = document.querySelector(".recipe");
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await res.json();
+        console.log(res, data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
